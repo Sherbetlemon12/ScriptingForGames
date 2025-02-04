4337904a1f84691d320e7274466cc47f73b6d8da
Ben <10804634@uvu.edu>
2025-02-04 08:34:20 -0700

    Added more movement and rotation to transformController script

--- AdventureGame/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ AdventureGame/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -6,10 +6,13 @@ public class TransformController : MonoBehaviour
     {
         // Move the target GameObject
         var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        var y = Mathf.PingPong(Time.time, 6);
+        var p = new Vector3(y, x, 0);
+        
         transform.position = p;
+        
 
         // Rotate the target GameObject
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(60, 90, 30) * Time.deltaTime);
     }
 }
